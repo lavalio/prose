8e9b2893a7ad186a3fc456c2ab65266ad5520a0a
lavalio <jz2326@gmail.com>
2022-02-01 21:43:55 -0500

    updated

--- join_test.go
+++ join_test.go
@@ -9,10 +9,10 @@ type testData struct {
 
 func TestJoinWithCommas(t *testing.T) {
 	tests := []testData{
-		testData{list: []string{}, want: ""},
-		testData{list: []string{"apple"}, want: "apple"},
-		testData{list: []string{"apple", "orange"}, want: "apple and orange"},
-		testData{list: []string{"apple", "orange", "pear"}, want: "apple, orange, and pear"},
+		{list: []string{}, want: ""},
+		{list: []string{"apple"}, want: "apple"},
+		{list: []string{"apple", "orange"}, want: "apple and orange"},
+		{list: []string{"apple", "orange", "pear"}, want: "apple, orange, and pear"},
 	}
 	for _, test := range tests {
 		got := JoinWithCommas(test.list)
